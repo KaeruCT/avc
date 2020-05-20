c0d6a5c8ea8de836c209d5b7c7a1b78bb2ef0eaa
Andrés Villarreal <villarreal@qinous.com>
2020-05-20 18:34:23 +0200

    Adding favicon

--- index.php
+++ index.php
@@ -31,6 +31,10 @@
     <meta property="keywords" content="Software Engineer, Frontend Developer, Software Development, Open Source, Web Developer"/>
     <link rel="alternate" href="//<?php echo $url; ?>" hreflang="en">
     <link rel="alternate" href="//<?php echo $url; ?>?es" hreflang="es">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 <body>
 
